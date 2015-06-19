4af48093f1e107c9e5553ad568b60734d9c3d526
Mic Alexander <micalexander@hotmail.com>
2015-06-19 13:28:21 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class dropzone {
   package { 'dropzone':
     provider => 'appdmg',
-    source => 'https://aptonic.com/dropzone3/latest/Dropzone-3.2.0.zip'
+    source => 'https://aptonic.com/dropzone3/latest/Dropzone-${version}.zip'
   }
 }
